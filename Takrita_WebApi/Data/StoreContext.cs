8e820f2ba1083edcc81a26481e809634f3cca94b
belha <belha@WAJIHBAHIJA>
2024-09-27 16:57:33 +0200

    Add project files.

---
+++ Takrita_WebApi/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Takrita_WebApi.Entities;
+
+namespace Takrita_WebApi.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product>  Products { get; set; }
+    }
+}
